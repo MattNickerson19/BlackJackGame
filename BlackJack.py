7bb17b08891f6b943b7902966e7f8ae88f953b2a
MattNickerson19 <matt.nickerson96@gmail.com>
2021-06-25 13:48:35 -0230

    "Added list for deck and money csv"

--- BlackJack.py
+++ BlackJack.py
@@ -5,16 +5,51 @@ import random
 
 def displayMenu():
     print("BLACKJACK!")
-    print("BlackJack payout is")
-    print()
-   
+    print("BlackJack payout is 3:2")
+    
 
+def dealerHand(deck):
+    dealerHand = []
+    dealCard = random.randint(0,len(deck)-1)
+    dealerCard = deck[dealCard]
+    dealerHand.append(dealerCard)
+    print("\nDEALER'S SHOW CARD:")
+    for card in dealerHand:
+            print(card[0],"of", card[1])
+    
 
-def dealerHand():
-    pass
+def playerHand(deck):
+    playerHand = []
+    
+    i = 0
+    while i < 2:
+        dealCard = random.randint(0,len(deck)-1)
+        playerCard = deck[dealCard]
+        playerHand.append(playerCard)
+        i += 1
+    print("\nPLAYER'S CARDS:")
+    for card in playerHand:
+            print(card[0],"of", card[1])
+            
+    playerMove = input("\nHit or Stand? (hit/stand):  ")
+    while True:
+        
+        if playerMove.lower() == "hit":
+            dealCard = random.randint(0,len(deck)-1)
+            playerCard = deck[dealCard]
+            playerHand.append(playerCard)
+            print("\nPLAYER'S CARDS:")
+            for card in playerHand:
+                print(card[0],"of", card[1])
+            playerMove = input("\nHit or Stand? (hit/stand):  ")
 
-def playerHand():
-    pass
+        elif playerMove.lower() == "stand":
+            break
+        
+        else:
+            print("Not valid Entry")
+            playerMove = input("\nHit or Stand? (hit/stand):  ")
+
 
 def playGame():
     pass
@@ -33,7 +68,8 @@ def readFile():
                 moneyAmount.append(row)
             return moneyAmount
 
-def deck():
+
+def main():
     deck = [["1","hearts",1],["1","diamonds",1],["1","clubs",1],["1","spades",1],
                   ["2","hearts",2], ["2","diamonds",1],["2","clubs",1],["2","spades",2],
                   ["3","hearts",3], ["3","diamonds",1],["3","clubs",1],["3","spades",3],
@@ -43,24 +79,18 @@ def deck():
                   ["7","hearts",7], ["7","diamonds",1],["7","clubs",1],["7","spades",7],
                   ["8","hearts",8], ["8","diamonds",1],["8","clubs",1],["8","spades",8],
                   ["9","hearts",9], ["9","diamonds",1],["9","clubs",1],["9","spades",9],
-                  ["10","hearts"[10],["10","diamonds",1],["10","clubs",1],["10","spades",10],
+                  ["10","hearts",10],["10","diamonds",1],["10","clubs",1],["10","spades",10],
                   ["Jack","hearts",10],["Jack","diamonds",1],["Jack","clubs",1],["Jack","spades",10],
                   ["Queen","hearts",10],["Queen","diamonds",1],["Queen","clubs",1],["Queen","spades",10],
                   ["King","hearts",10],["King","diamonds",1],["King","clubs",1],["King","spades",10],
-                  ["Ace","hearts",1,11],["Ace","diamonds",1,11],["Ace","clubs",1,11],["Ace","spades",1,11],]
-    
-    return deck
+                  ["Ace","hearts",1,11],["Ace","diamonds",1,11],["Ace","clubs",1,11],["Ace","spades",1,11]]
 
-def main():
     displayMenu()
     moneyAmount = readFile()
-    deck()
+    dealerHand(deck)
+    playerHand(deck)
     
-   
     
-   
-
-
 
 if __name__ == "__main__":
     main()
