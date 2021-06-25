f99b1887ccb24392330d557878f4c65e1d766659
MattNickerson19 <matt.nickerson96@gmail.com>
2021-06-24 22:53:10 -0230

    Created repo and pseudocode

---
+++ BlackJack.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import csv
+import random
+
+def displayMenu():
+    pass
+
+def dealerHand():
+    pass
+
+def playerHand():
+    pass
+
+def playGame():
+    pass
+
+def writeFile():
+    pass
+
+def readFile():
+    pass
+
+def main()
+    displayMenu()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
